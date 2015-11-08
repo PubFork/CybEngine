92e28238f0f6367270119113737dd8324152ff24
Jezper Blixt <jezper.blixt@gmail.com>
2015-11-08 23:54:34 +0100

    Cleaning

--- make_build.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-mkdir build
-chdir build
-cmake -Wdev -G "Visual Studio 14" ../
-pause
--- make_clean.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Removing build directory...
-rmdir /S /Q build 2> nul
-
-echo Removing bin directory...
-rmdir /S /Q bin 2> nul
-
-echo Done.
